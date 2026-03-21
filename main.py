6a97f9571bf110c048c55d1f973f92297a8e079c
Fabian <fabianmoraesb@gmail.com>
2026-03-21 22:21:35 +0100

    docling testing for parsing

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import pymupdf
 from gliner2 import GLiNER2
 
 # TODO: ulepszyc to jak wyjmujemy inforamcje zeby latwiej (i z wieksza skutecznoscia) gliner2 je dopasowywal
-# TODO: extract danych osobwych na start
 # TODO: testy  duzo testow
-# TODO: 
+# TODO!: co z OCR?
 
 extractor = GLiNER2.from_pretrained("fastino/gliner2-multi-v1")
 
@@ -16,13 +15,19 @@ def extract_text_pymupdf(pdf_path):
             page_text = page.get_text("text")
             if not isinstance(page_text, str):
                 page_text = str(page_text)
-            text += page_text  # plain text
+            text += page_text 
         return text
     finally:
         doc.close()
 
+def get_clean_text(pdf: str): 
 
-# Extract text from a PDF.
+    text = extract_text_pymupdf(f"pdfs/{pdf}.pdf")
+    labels_en = ["Experience", "Skills", "Education", "Full Name", "Email", "date and location of birth",
+                "phone number", "adress"]
+    result = extractor.extract_entities(text, labels_en)
+    return result
+
 text = extract_text_pymupdf("pdfs/pdf2.pdf")
-result = extractor.extract_entities(text, ["Umiejętności", "Zainteresowania", "Języki", "Wykształcenie", "Dane osobowe"])
-print(result)
+print(text)
+print(get_clean_text("pdf2"))
