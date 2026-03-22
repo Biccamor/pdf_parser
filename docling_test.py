5189cf10f2a4e45f258131c953ac0beae8811ab2
Fabian <fabianmoraesb@gmail.com>
2026-03-22 16:49:31 +0100

    skelet for main and compose file

--- docling_test.py
+++ docling_test.py
@@ -3,6 +3,8 @@ from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.datamodel.base_models import InputFormat
 
 def parse(sciezka_do_pdf):
+
+    
     pipeline_options = PdfPipelineOptions()
     pipeline_options.do_table_structure = False 
     
@@ -15,4 +17,4 @@ def parse(sciezka_do_pdf):
     
     return wynik.document.export_to_markdown()
 
-print(parse("pdfs/pdf1.pdf"))
+print(parse("pdfs/pdf3.pdf"))
--- main.py
+++ main.py
@@ -1,33 +1,7 @@
-import pymupdf
-from gliner2 import GLiNER2
+from fastapi import FastAPI
 
-# TODO: ulepszyc to jak wyjmujemy inforamcje zeby latwiej (i z wieksza skutecznoscia) gliner2 je dopasowywal
-# TODO: testy  duzo testow
-# TODO!: co z OCR?
+app = FastAPI()
 
-extractor = GLiNER2.from_pretrained("fastino/gliner2-multi-v1")
-
-def extract_text_pymupdf(pdf_path):
-    doc = pymupdf.open(pdf_path)
-    try:
-        text = ""
-        for page in doc:
-            page_text = page.get_text("text")
-            if not isinstance(page_text, str):
-                page_text = str(page_text)
-            text += page_text 
-        return text
-    finally:
-        doc.close()
-
-def get_clean_text(pdf: str): 
-
-    text = extract_text_pymupdf(f"pdfs/{pdf}.pdf")
-    labels_en = ["Experience", "Skills", "Education", "Full Name", "Email", "date and location of birth",
-                "phone number", "adress"]
-    result = extractor.extract_entities(text, labels_en)
-    return result
-
-text = extract_text_pymupdf("pdfs/pdf2.pdf")
-print(text)
-print(get_clean_text("pdf2"))
+@app.post("/parser")
+async def main():
+    ... 
